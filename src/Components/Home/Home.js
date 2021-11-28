aba6a5a81ace1a996073dbe485a2c04d263aaf66
Shuvo <74643721+AhmdShuvo@users.noreply.github.com>
2021-11-29 01:07:48 +0600

    first commit

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { Container } from '@mui/material';
+import BasicModal from './Modal/Modal';
+
+const Home = () => {
+    return (
+        <Container>
+            <h4> resume</h4>
+
+            <BasicModal></BasicModal>        
+        </Container>
+    );
+};
+
+export default Home;
---
+++ src/Components/Home/Modal/Resume/Resume.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Resume = () => {
+    return (
+       <div>
+            <iframe src="./Resume/Tanjir-Ahmed- Resume.pdf" width="100%"/>
+       </div>
+
+    )
+}
+
+export default Resume;
